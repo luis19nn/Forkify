cd92dbef21966c7370cec28c5427a52caf81f235
Luis Ferreira <luis19nn@gmail.com>
2021-12-15 23:42:30 -0300

    Forkify enviado

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'ab9f8fc1-fc41-46b2-bfc4-ec970e06bd0f';
+export const MODAL_CLOSE_SEC = 2.5;
